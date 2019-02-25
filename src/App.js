efb64c6380c9c06e4a6009eb14e3ca940e73071e
simonghtang <simon.gh.tang@gmail.com>
2019-02-25 19:31:52 +1100

    added dropdown, left menu pictures list added, column correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
+
+import Modules from './components/modules/modules';
+
 import './App.css';
 
 class App extends Component {
@@ -8,16 +11,16 @@ class App extends Component {
       <div>
         <Grid>
           <Grid.Row>
-            <Grid.Column width={4} >
-              <Segment className='segment' color='blue'>Blue</Segment>
+            <Grid.Column width={3} >
+              <Modules/>
             </Grid.Column>
 
-            <Grid.Column width={7}>
-              <Segment className='segment' color='green'>Green</Segment>
+            <Grid.Column width={10}>
+              <Segment color='green'>Green</Segment>
             </Grid.Column >
 
-            <Grid.Column width={5}>
-              <Segment className='segment' color='red'>Red</Segment>
+            <Grid.Column width={3}>
+              <Segment color='red'>Red</Segment>
             </Grid.Column>
           </Grid.Row>
         </Grid>
--- src/components/modules/modules.js
+++ src/components/modules/modules.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 
-import { Sidebar, Menu, Divider, Image } from 'semantic-ui-react';
+import { Menu, Divider, Image, Dropdown } from 'semantic-ui-react';
 
-export default class Modules extends Component {
-    render() {
-        return (
-            <div>
-                
-                    <Menu vertical>
-                        <Menu.Item>
-                            <Image src='https://react.semantic-ui.com/images/wireframe/image.png' size='mini' />
-                        </Menu.Item>
-                        <Menu.Item>
-                            <Image src='https://react.semantic-ui.com/images/wireframe/image.png' size='mini' />
-                        </Menu.Item>
-                        <Menu.Item>
-                            <Image src='https://react.semantic-ui.com/images/wireframe/image.png' size='mini' />
-                        </Menu.Item>
-                        <Divider/>
-                    </Menu>
-            </div>
-        )
-    }
+function Modules () {
+    return (
+        <div>
+            
+                <Menu vertical>
+                    <Menu.Item>
+                        <Dropdown fluid selection/>
+                    </Menu.Item>
+                    <Divider/>
+                    <Menu.Item>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' size='mini' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' size='mini' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' size='mini' />
+                    </Menu.Item>
+                    <Divider/>
+                </Menu>
+        </div>
+    )
 }
+
+export default Modules;
 
