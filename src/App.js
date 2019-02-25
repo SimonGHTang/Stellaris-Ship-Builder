257a09173f7bfcc29fd1fdc4ac81bc932f384d65
simonghtang <simon.gh.tang@gmail.com>
2019-02-25 19:11:50 +1100

    css working

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import React, { Component } from 'react';
-import { Sidebar, Menu, Image, Header, Icon, Segment } from 'semantic-ui-react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Grid, Segment } from 'semantic-ui-react';
 import './App.css';
-
-import Modules from './components/modules/modules';
-
-import shipSelect from './components/builder/ship-select';
-import corvetteBuilder from './components/builder/corvette-builder';
-
-import ShipStats from './components/ship-stats/ship-stats';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Sidebar.Pushable>
-          <Sidebar as={Menu} direction='left' vertical width='thin'>
-            <Menu.Item> 
-              <Modules />
-            </Menu.Item>
-          </Sidebar>
-        
-          <Sidebar.Pusher width="80%">
-            <BrowserRouter>
-              <Switch>
-                <Route exact path = "/" component = {shipSelect} />
-                <Route path = "/corvette-builder" component = {corvetteBuilder} />
-                <Route component = {shipSelect} />
-              </Switch>
-            </BrowserRouter>
-          </Sidebar.Pusher>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={4} >
+              <Segment className='segment' color='blue'>Blue</Segment>
+            </Grid.Column>
 
-          <Sidebar as={Menu} direction='right' vertical>
-            <ShipStats/>
-          </Sidebar>
-        </Sidebar.Pushable>
+            <Grid.Column width={7}>
+              <Segment className='segment' color='green'>Green</Segment>
+            </Grid.Column >
+
+            <Grid.Column width={5}>
+              <Segment className='segment' color='red'>Red</Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
